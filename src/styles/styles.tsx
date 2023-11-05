2ce08227777454167e1ed3333e5ca7a9f9092baa
WesleyVasconcelos1218 <wesley.vasconcelos@petzpartner.com.br>
2023-11-05 13:53:57 -0300

    added media

--- src/styles/styles.tsx
+++ src/styles/styles.tsx
@@ -2,5 +2,8 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   margin: 4px auto 0 auto;
+  @media (max-width: 700px) {
+    padding: 0 10px; 
+  }
 `;
 
